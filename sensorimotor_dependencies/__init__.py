1bc389b4a744f0b947440f779d7b3f88eb474871
youkad <youqad@gmail.com>
2018-06-08 16:49:33 +0200

    init file

---
+++ sensorimotor_dependencies/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.1.1'
+__name__ = 'sensorimotor_dependencies'
+__author__ = 'Kexin Ren and Younesse Kaddar'
+__credits__ = ['Kexin Ren', 'Younesse Kaddar', 'David Philipona', 
+            'J.K. O\'Regan', 'Jean-Pierre Nadal']
+__email__ = 'youqad@gmail.com'
+__github__ = 'https://github.com/youqad/Neurorobotics_Project'
